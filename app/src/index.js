830ecedca168593d09efc171001db4af8c4816e3
Saamu192 <samuelpr192@gmail.com>
2022-10-19 09:07:50 -0300

    feat: main script

--- app/src/index.js
+++ app/src/index.js
@@ -2,10 +2,16 @@
 const { readFile } = require("fs/promises");
 const { join } = require("path");
 const pdf = require("pdf-parse");
+const TextProcessorFacade = require("./textProcessorFacade");
 
 (async () => {
   const dataBuffer = await readFile(
     join(__dirname, "./../../docs/contrato.pdf")
   );
-  const data = (await pdf(dataBuffer)).text;
+
+  const data = await pdf(dataBuffer);
+
+  const instance = new TextProcessorFacade(data.text);
+  const people = instance.getPeopleFromPDF();
+  console.log("people", people);
 })();
